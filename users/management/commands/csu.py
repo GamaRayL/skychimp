32c8965c58bf7fbb65bcc46bb7b0da96aff465b9
Gamid <gamaizingg@gmail.com>
2023-10-17 12:57:20 +0300

    Команда для создания суперпользователя

---
+++ users/management/commands/csu.py
@@ -0,0 +1,11 @@
+from users.models import User
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+    """Команда для создания суперпользователя"""
+
+    def handle(self, *args, **options):
+        user = User.objects.create(email='admin@gmail.com', is_superuser=True, is_staff=True)
+        user.set_password('admin')
+        user.save()
